b86fbd2f077ad96a2cb0e8f0adb5cf93af61988f
BONG <bong@BONGui-MacBookPro.local>
2023-01-29 17:08:51 +0900

    2023.01.29 LBS: add comment

--- 귤 고르기/solution.py
+++ 귤 고르기/solution.py
@@ -5,8 +5,11 @@ def solution(k, tangerine):
     answer = 0
     total = 0
 
+    # Counter를 이용해서 각 무게별로 몇 개의 귤이 있는지 카운트 합니다.
+    # 카운트한 결과에서 갯수가 많은 순으로 sort한 값을 리스트를 만든다.
     values = sorted(Counter(tangerine).values(), reverse=True)
 
+    # 갯수가 많은 종류별로 더해가면서 원하는 갯수 이상인지를 판별하면 이상이 되었을 때 반복문을 break한다.
     for v in values:
         total += v
         answer += 1
